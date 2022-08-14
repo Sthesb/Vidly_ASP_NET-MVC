b333649998ee0ed6f795289a39e8145ce86140e5
Sthesb <smathebula.sb@gmail.com>
2022-08-14 11:55:15 +0200

    Display the list of customers and movie

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -11,28 +11,22 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        
-
-        // random
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            var movie = new Movie { Id = 1, Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Sthembiso"},
-                new Customer { Name = "Goodness"},
-            };
+            var movies = GetMovies();
 
-            var viewModel = new RandomViewModel 
-            {
-                Movie = movie,
-                Customers = customers,
-            };
-
-            return View(viewModel);
+            return View(movies);
         }
 
-        
-        
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+
     }
 }
